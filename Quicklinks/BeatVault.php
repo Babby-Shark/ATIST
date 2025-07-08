6468f90ef3f2392be1790e77efc175d0726d004c
Babby-Shark <159906571+Babby-Shark@users.noreply.github.com>
2025-07-08 13:07:18 +0000

    partials and BeatVault

---
+++ Quicklinks/BeatVault.php
@@ -0,0 +1,140 @@
+
+<html>
+  <head>
+      <link rel="stylesheet" href="../CSS/main.css">
+  </head>
+  <body>
+      <?php include('../partials/header.php'); ?>
+
+      <div data-layer="Landing page" class="LandingPage" style="width: 1440px; height: 4811px; position: relative; background: white; overflow: hidden">
+        <div data-layer="Hero Image" class="HeroImage" style="width: 1280px; height: 640px; left: 80px; top: 581px; position: absolute; overflow: hidden; border-radius: 8px; background-image: url()">
+            <img data-layer="gang in the stu.... album in the works 🔥 1" class="GangInTheStuAlbumInTheWorks1" style="width: 1280px; height: 1280px; left: 0px; top: -586px; position: absolute" src="https://placehold.co/1280x1280" />
+        </div>
+      <div data-layer="Featured Beats" class="FeaturedBeats" style="width: 624px; left: 80px; top: 1371px; position: absolute; color: black; font-size: 48px; font-family: Inter; font-weight: 600; word-wrap: break-word">
+          Featured Beats
+      </div>
+      <div data-layer="Frame 1321317456" class="Frame1321317456" style="width: 1279px; left: 80px; top: 1477px; position: absolute; justify-content: flex-start; align-items: center; gap: 32px; display: inline-flex">
+        <div data-layer="Card" class="Card" style="flex: 1 1 0; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+          <img data-layer="Image" class="Image" style="align-self: stretch; height: 405px; position: relative; border-radius: 8px" src="https://placehold.co/405x405" />
+          <div data-layer="Copy" class="Copy" style="width: 381px; flex-direction: column; justify-content: center; align-items: flex-start; gap: 4px; display: flex">
+            <div data-layer="Subheading" class="Subheading" style="align-self: stretch; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+              Subheading
+            </div>
+          </div>
+      </div>
+      <div data-layer="Card" class="Card" style="flex: 1 1 0; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+          <img data-layer="Image" class="Image" style="align-self: stretch; height: 405px; position: relative; border-radius: 8px" src="https://placehold.co/405x405" />
+          <div data-layer="Copy" class="Copy" style="width: 381px; flex-direction: column; justify-content: center; align-items: flex-start; gap: 4px; display: flex">
+            <div data-layer="Subheading" class="Subheading" style="align-self: stretch; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+              Subheading
+            </div>
+          </div>
+      </div>
+      <div data-layer="Card" class="Card" style="flex: 1 1 0; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+          <img data-layer="Image" class="Image" style="align-self: stretch; height: 405px; position: relative; border-radius: 8px" src="https://placehold.co/405x405" />
+          <div data-layer="Copy" class="Copy" style="width: 381px; flex-direction: column; justify-content: center; align-items: flex-start; gap: 4px; display: flex">
+          <div data-layer="Subheading" class="Subheading" style="align-self: stretch; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+            Subheading
+          </div>
+          </div>
+        </div>
+      </div>
+      <img data-layer="Image" class="Image" style="width: 704px; height: 704px; left: 736px; top: 2198px; position: absolute; border-top-left-radius: 8px; border-bottom-left-radius: 8px" src="https://placehold.co/704x704" />
+      <div data-layer="Browse & Buy" class="BrowseBuy" style="width: 624px; left: 80px; top: 3082px; position: absolute; color: black; font-size: 48px; font-family: Inter; font-weight: 600; word-wrap: break-word">
+        Browse & Buy
+      </div>
+      <div data-layer="Cards" class="Cards" style="width: 1280px; left: 80px; top: 3188px; position: absolute; justify-content: flex-start; align-items: center; gap: 31px; display: inline-flex">
+        <div data-layer="Card" class="Card" style="flex: 1 1 0; overflow: hidden; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 32px; display: inline-flex">
+          <img data-layer="Image" class="Image" style="align-self: stretch; height: 341px; position: relative; border-radius: 8px" src="https://placehold.co/624x341" />
+          <div data-layer="Copy" class="Copy" style="width: 548.44px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: flex">
+            <div data-layer="Subheading" class="Subheading" style="width: 548px; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+              Subheading
+            </div>
+            <div data-layer="Rectangle" class="Rectangle" style="width: 603.50px; height: 54px; background: #D9D9D9"></div>
+          </div>
+        </div>
+        <div data-layer="Card" class="Card" style="flex: 1 1 0; overflow: hidden; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 32px; display: inline-flex">
+          <img data-layer="Image" class="Image" style="align-self: stretch; height: 341px; position: relative; border-radius: 8px" src="https://placehold.co/624x341" />
+          <div data-layer="Copy" class="Copy" style="width: 547.56px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: flex">
+            <div data-layer="Subheading" class="Subheading" style="width: 548px; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">Subheading</div>
+            <div data-layer="Body text for whatever you’d like to expand on the main point." class="BodyTextForWhateverYouDLikeToExpandOnTheMainPoint" style="width: 548px; justify-content: center; display: flex; flex-direction: column; color: #828282; font-size: 24px; font-family: Inter; font-weight: 400; line-height: 36px; word-wrap: break-word">Body text for whatever you’d like to expand on the main point. </div>
+          </div>
+        </div>
+      </div>
+      <div data-layer="Cards" class="Cards" style="width: 1280px; left: 80px; top: 3749px; position: absolute; justify-content: flex-start; align-items: center; gap: 31px; display: inline-flex">
+        <div data-layer="Card" class="Card" style="flex: 1 1 0; overflow: hidden; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 32px; display: inline-flex">
+          <img data-layer="Image" class="Image" style="align-self: stretch; height: 341px; position: relative; border-radius: 8px" src="https://placehold.co/624x341" />
+          <div data-layer="Copy" class="Copy" style="width: 548.44px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: flex">
+            <div data-layer="Subheading" class="Subheading" style="width: 548px; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+              Subheading
+            </div>
+            <div data-layer="Rectangle" class="Rectangle" style="width: 603.50px; height: 54px; background: #D9D9D9"></div>
+          </div>
+        </div>
+        <div data-layer="Card" class="Card" style="flex: 1 1 0; overflow: hidden; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 32px; display: inline-flex">
+          <img data-layer="Image" class="Image" style="align-self: stretch; height: 341px; position: relative; border-radius: 8px" src="https://placehold.co/624x341" />
+          <div data-layer="Copy" class="Copy" style="width: 547.56px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: flex">
+            <div data-layer="Subheading" class="Subheading" style="width: 548px; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+              Subheading
+            </div>
+            <div data-layer="Body text for whatever you’d like to expand on the main point." class="BodyTextForWhateverYouDLikeToExpandOnTheMainPoint" style="width: 548px; justify-content: center; display: flex; flex-direction: column; color: #828282; font-size: 24px; font-family: Inter; font-weight: 400; line-height: 36px; word-wrap: break-word">
+              Body text for whatever you’d like to expand on the main point.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div data-layer="Custom Beat Requests" class="CustomBeatRequests" style="width: 624px; left: 80px; top: 2198px; position: absolute; color: black; font-size: 48px; font-family: Inter; font-weight: 600; word-wrap: break-word">
+        Custom Beat Requests
+      </div>
+      <div data-layer="Text" class="Text" style="width: 516px; left: 79px; top: 2304px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-end; gap: 48px; display: inline-flex">
+        <div data-layer="Text" class="Text" style="align-self: stretch; flex-direction: column; justify-content: center; align-items: flex-start; gap: 8px; display: flex">
+          <div data-layer="Tell Us Your Sound" class="TellUsYourSound" style="width: 515px; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+            Tell Us Your Sound
+          </div>
+          <div data-layer="Describe the vibe, and share any reference tracks." class="DescribeTheVibeAndShareAnyReferenceTracks" style="width: 515px; justify-content: center; display: flex; flex-direction: column; color: #828282; font-size: 24px; font-family: Inter; font-weight: 400; line-height: 36px; word-wrap: break-word">Describe the vibe, and share any reference tracks.</div>
+        </div>
+        <div data-layer="Text" class="Text" style="align-self: stretch; flex-direction: column; justify-content: center; align-items: flex-start; gap: 8px; display: flex">
+          <div data-layer="Connect with a Producer" class="ConnectWithAProducer" style="width: 515px; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+            Connect with a Producer
+          </div>
+          <div data-layer="We'll match you with a BeatVault producer you can talk to directly—online or in person." class="WeLlMatchYouWithABeatvaultProducerYouCanTalkToDirectlyOnlineOrInPerson" style="width: 515px; justify-content: center; display: flex; flex-direction: column; color: #828282; font-size: 24px; font-family: Inter; font-weight: 400; line-height: 36px; word-wrap: break-word">
+            We'll match you with a BeatVault producer you can talk to directly—online or in person.
+          </div>
+        </div>
+        <div data-layer="Text" class="Text" style="align-self: stretch; flex-direction: column; justify-content: center; align-items: flex-start; gap: 8px; display: flex">
+          <div data-layer="Get & Refine Your Beat" class="GetRefineYourBeat" style="align-self: stretch; color: black; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+            Get & Refine Your Beat
+          </div>
+          <div data-layer="Receive your custom-made beat in your preferred format. Need tweaks? No problem—request adjustments until it fits just right." class="ReceiveYourCustomMadeBeatInYourPreferredFormatNeedTweaksNoProblemRequestAdjustmentsUntilItFitsJustRight" style="align-self: stretch; justify-content: center; display: flex; flex-direction: column; color: #828282; font-size: 24px; font-family: Inter; font-weight: 400; line-height: 36px; word-wrap: break-word">
+            Receive your custom-made beat in your preferred format. Need tweaks? No problem—request adjustments until it fits just right.
+          </div>
+        </div>
+      </div>
+      <div data-layer="Buttons" class="Buttons" style="left: 80px; top: 2832px; position: absolute; justify-content: flex-start; align-items: center; gap: 16px; display: inline-flex">
+        <div data-layer="Button" class="Button" style="padding-left: 24px; padding-right: 24px; padding-top: 12px; padding-bottom: 12px; background: black; box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05); border-radius: 8px; justify-content: flex-start; align-items: center; gap: 8px; display: flex">
+          <div data-layer="Try It" class="TryIt" style="justify-content: center; display: flex; flex-direction: column; color: white; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+            Try It
+          </div>
+        </div>
+      </div>
+      <div data-layer="Copy" class="Copy" style="width: 844px; left: 79px; top: 212px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 40px; display: inline-flex">
+        <div data-layer="Page title" class="PageTitle" style="align-self: stretch; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: flex">
+          <div data-layer="Unlock the Sound, Welcome to BeatVault." class="UnlockTheSoundWelcomeToBeatvault" style="align-self: stretch; color: black; font-size: 64px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+            Unlock the Sound, Welcome to BeatVault.
+          </div>
+          <div data-layer="Browse fire beats or request your own custom sound. BeatVault is where artists come to grab their soul merging beats." class="BrowseFireBeatsOrRequestYourOwnCustomSoundBeatvaultIsWhereArtistsComeToGrabTheirSoulMergingBeats" style="align-self: stretch; justify-content: center; display: flex; flex-direction: column; color: rgba(0, 0, 0, 0.75); font-size: 24px; font-family: Inter; font-weight: 400; line-height: 36px; word-wrap: break-word">
+            Browse fire beats or request your own custom sound. BeatVault is where artists come to grab their soul merging beats.
+          </div>
+        </div>
+        <div data-layer="Button" class="Button" style="padding-left: 32px; padding-right: 32px; padding-top: 20px; padding-bottom: 20px; background: black; box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05); border-radius: 8px; justify-content: flex-start; align-items: center; gap: 8px; display: inline-flex">
+          <div data-layer="Button" class="Button" style="justify-content: center; display: flex; flex-direction: column; color: white; font-size: 24px; font-family: Inter; font-weight: 500; line-height: 36px; word-wrap: break-word">
+            Button
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include('../partials/footer.php'); ?>
+
+  </body>
+</html>
