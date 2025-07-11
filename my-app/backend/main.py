1acc731ba23f4a1a89181daddeacfd9092ed4b75
Babby-Shark <159906571+Babby-Shark@users.noreply.github.com>
2025-07-11 08:43:50 +0000

    postgre&docker2

--- my-app/backend/main.py
+++ my-app/backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from database import engine, metadata
+from image_routes import router as image_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -14,3 +17,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+metadata.create_all(bind=engine)
+
+app.mount("/images", StaticFiles(directory="images"), name="images")
+app.include_router(image_router)
